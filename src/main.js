b3df1e336492b23427253765c16842e383c31bd6
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-12-20 23:41:26 +0200

    add open and close for mobile menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
+(() => {
+  const modal = document.getElementById('myModal');
+  const openMenu = document.getElementById('open');
+  const closeMenu = document.getElementById('close');
 
+  openMenu.onclick = function () {
+    modal.style.display = 'block';
+  };
+
+  closeMenu.onclick = function () {
+    modal.style.display = 'none';
+  };
+
+  // When the user clicks anywhere outside of the modal menu, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
+})();
