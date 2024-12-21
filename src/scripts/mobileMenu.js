debed32d32d1f3263ca0864d6fddce981dd01fb4
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-12-21 15:24:35 +0200

    modal menu fixing

--- src/scripts/mobileMenu.js
+++ src/scripts/mobileMenu.js
@@ -2,19 +2,20 @@
   const modal = document.getElementById('myModal');
   const openMenu = document.getElementById('open');
   const closeMenu = document.getElementById('close');
+  const linkMenu = document.getElementById('link');
 
   openMenu.onclick = function () {
     modal.style.display = 'block';
   };
 
-  closeMenu.onclick = function () {
+  const closeModal = () => {
     modal.style.display = 'none';
   };
 
-  // When the user clicks anywhere outside of the modal menu, close it
+  closeMenu.onclick = closeModal;
+  linkMenu.onclick = closeModal;
+
   window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = 'none';
-    }
+    if (event.current.target !== openMenu) closeModal;
   };
 })();
