0fdca757d356be4886cb3654987f05e7c4b5f323
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-12-22 14:38:08 +0200

    section advantages

--- src/scripts/mobileMenu.js
+++ src/scripts/mobileMenu.js
@@ -1,15 +1,15 @@
 (() => {
   const modal = document.getElementById('myModal');
   const openMenu = document.getElementById('open');
-  const closeMenu = document.getElementById('close');
+  const closeMenu = document.getElementsByClassName('closeBtn');
   const linkMenu = document.getElementById('link');
 
   openMenu.onclick = function () {
-    modal.style.display = 'block';
+    modal.classList.remove('hidden');
   };
 
   const closeModal = () => {
-    modal.style.display = 'none';
+    modal.classList.add('hidden');
   };
 
   closeMenu.onclick = closeModal;
