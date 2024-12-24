0015c1c5d4d571bbed9d17b7400f3161cee0ad1f
defaultnastia <anastayaaa@gmail.com>
2024-12-24 14:56:19 +0100

    fixed mobile menu position

--- src/scripts/mobileMenu.js
+++ src/scripts/mobileMenu.js
@@ -1,26 +1,37 @@
 (() => {
   const modal = document.getElementById('myModal');
+  const modalBackdrop = document.getElementById('modalBackdrop');
   const openMenu = document.getElementById('open');
   const closeMenu = document.getElementById('close');
   const linkItem = document.getElementsByClassName('mobileMenuLink');
 
+  const displayMobileMenu = isDisplayed => {
+    if (isDisplayed) {
+      modal.style.display = 'flex';
+      modalBackdrop.style.display = 'block';
+    } else {
+      modal.style.display = 'none';
+      modalBackdrop.style.display = 'none';
+    }
+  };
+
   openMenu.onclick = function () {
-    modal.style.display = 'block';
+    displayMobileMenu(true);
   };
 
   closeMenu.onclick = function () {
-    modal.style.display = 'none';
+    displayMobileMenu(false);
   };
 
   Array.from(linkItem).forEach(element => {
     element.onclick = function () {
-      modal.style.display = 'none';
+      displayMobileMenu(false);
     };
   });
 
   window.onclick = function (event) {
     if (!modal.contains(event.target) && !openMenu.contains(event.target)) {
-      modal.style.display = 'none';
+      displayMobileMenu(false);
     }
   };
 })();
